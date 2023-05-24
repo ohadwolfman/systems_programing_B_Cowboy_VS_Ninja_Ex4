21bb3fbe5b5743ca081b7fcd3f6a5940c285fc79
Ohad Wolfman <98156296+ohadwolfman@users.noreply.github.com>
2023-05-24 09:49:14 +0300

    #bash grade 100

--- Test.cpp
+++ Test.cpp
@@ -27,22 +27,22 @@ TEST_CASE("Cowboy and Ninja classes") {
     CHECK(n1.distance(&c1) == c1.distance(&n1));
 
     int powerBefore = c2.getPower();
-    n1.slash(c2);
+    n1.slash(&c2);
     CHECK(c2.getPower() == powerBefore);
-    n1.move(c2);
-    n1.slash(c2);
+    n1.move(&c2);
+    n1.slash(&c2);
     CHECK_FALSE(c2.getPower() == powerBefore);
 
     for(size_t i = 0; i<5; i++){ // Old ninja has 150 power pints
-        c1.shoot(n2);
-        c2.shoot(n2);
-        c3.shoot(n2);
+        c1.shoot(&n2);
+        c2.shoot(&n2);
+        c3.shoot(&n2);
     }
     CHECK_FALSE(n2.isAlive());
     CHECK(c1.hasbullets());
     CHECK(c2.hasbullets());
     CHECK(c3.hasbullets());
-    c1.shoot(n1);
+    c1.shoot(&n1);
     CHECK(c1.hasbullets());
 
     CHECK(c2.getName() == "Ron");
@@ -50,11 +50,11 @@ TEST_CASE("Cowboy and Ninja classes") {
     c3.setName("Tom_Riddle");
     CHECK(c3.getName() == "Tom_Riddle");
 
-    CHECK(n1.print() == "Name: Harry, Hit Points: 110, Location: (12.0,12.0)");
-    CHECK(n2.print() == "Name: Dambeldor, Location: (6.0,8.0)");
-    CHECK(c1.print() == "Name: Hermione, Hit Points: 110, Location: (4.0,6.0)");
-    CHECK(c2.print() == "Name: Ron, Hit Points: 70, Location: (12.0,12.0)");
-    CHECK(c3.print() == "Name: Tom_Riddle, Hit Points: 110, Location: (10.0,1.0)");
+    CHECK(n1.print() == "N: Name: Harry, Hit Points: 110, Location: (12.000000,12.000000)");
+    CHECK(n2.print() == "N: Name: Dambeldor, Location: (6.000000,8.000000)");
+    CHECK(c1.print() == "C: Name: Hermione, Hit Points: 110, Location: (4.000000,6.000000)");
+    CHECK(c2.print() == "C: Name: Ron, Hit Points: 70, Location: (12.000000,12.000000)");
+    CHECK(c3.print() == "C: Name: Tom_Riddle, Hit Points: 110, Location: (10.000000,1.000000)");
 }
 
 //TEST_CASE("Test Team class") {
